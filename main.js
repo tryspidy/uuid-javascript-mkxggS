c3672d0c76bb3aa5e58715d996eeb02af7dc4c3a
tryspidy <68438592+tryspidy@users.noreply.github.com>
2021-10-06 12:21:02 +0530

    Initial commit

---
+++ main.js
@@ -0,0 +1,11 @@
+    function uuid() {
+        const template = 'xxxxxxxx-xxxx-4xxx-yxxx-xxxxxxxxxxxx';
+        const xAndYOnly = /[xy]/g;
+
+        return template.replace(xAndYOnly, (character) => {
+            const randomNo =  Math.floor(Math.random() * 16);
+            const newValue = character === 'x' ? randomNo : (randomNo & 0x3) | 0x8;
+
+            return newValue.toString(16);
+        });
+    }
